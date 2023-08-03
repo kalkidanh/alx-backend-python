994d832adb689125739cf2c70c4c107ffb36b688
kalkidanh <kalkidanh89@gmail.com>
2023-08-03 20:02:54 +0300

    strings concatenated

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" A type-annotated function - concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates a string
+
+    Args:
+        str1 (str): String to concatenate
+        str2 (str): String to concatenate
+
+    Returns:
+        string: The concatenated string
+    """
+    return str1 + str2
