9765147ac884b230388a060f1fd1d203e1bb6504
kalkidanh <kalkidanh89@gmail.com>
2023-08-03 19:56:49 +0300

    floating sum returned

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" A type-annotated function - add."""
+
+
+def add(a: float, b: float) -> float:
+    """ The sum  of two floating point numbers
+
+    Args:
+        a (float): a floating point number
+        b (float): a floating point number
+
+    Returns:
+        float: Sum of a and b
+    """
+    return a + b
