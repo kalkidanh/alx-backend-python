496ff67ec2bcbbc0e604abe05ce8786e71f1f5be
kalkidanh <kalkidanh89@gmail.com>
2023-08-03 20:10:01 +0300

    string representation returned

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" A type-annotated function - floor."""
+
+
+def floor(n: float) -> int:
+    """ The floor of the given number
+
+    Args:
+        n (float): a floating point number
+
+    Returns:
+        Integer: the floor of the floating point number
+    """
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" A type-annotated function to_str."""
+
+
+def to_str(n: float) -> str:
+    """ Converts a floating point number to a string
+
+    Args:
+        n (float): a floating point number
+
+    Returns:
+        String: The str representation of of the number
+    """
+    return str(n)
